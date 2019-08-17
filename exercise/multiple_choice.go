34457ff57972fc9f103dd849f2476f163d0f2654
Hein Meling <hein.meling@uis.no>
2019-08-17 20:02:10 +0200

    revised the multiple choice code and add a tests
    also added go modules
    
    the test currently fails as it would when running student code.
    this is not ideal for testing purposes, but the code for the
    multiple choice is a bit hard to test in its current form.
    a revision shouldn't work on the individual file. it would
    probably be easier to work with a slice of strings (lines).
    but don't have time now to fix this.

--- exercise/multiple_choice.go
+++ exercise/multiple_choice.go
@@ -1,18 +1,17 @@
 package exercise
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"regexp"
 	"strconv"
 	"strings"
 	"testing"
-	"unicode/utf8"
 
 	"github.com/autograde/kit/score"
 )
 
+// Choices are the set of correct choices for the questions.
 type Choices []struct {
 	Number int
 	Want   rune
@@ -23,6 +22,7 @@ type Choices []struct {
 // in the answerKey object. The function requires a Score object, and
 // will produce both string output and JSON output.
 func MultipleChoice(t *testing.T, sc *score.Score, fileName string, answers Choices) {
+	t.Helper()
 	defer sc.WriteString(os.Stdout)
 	defer sc.WriteJSON(os.Stdout)
 
@@ -30,26 +30,35 @@ func MultipleChoice(t *testing.T, sc *score.Score, fileName string, answers Choi
 	bytes, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		sc.Score = 0
-		t.Fatalf(fmt.Sprintf("%v: error reading the file: %v", fileName, err))
+		t.Fatalf("%v: error reading the file: %v", fileName, err)
 		return
 	}
 
 	for i := range answers {
-		// Find the user's answer to the corresponding question number
-		regexStr := "\n" + strconv.Itoa(answers[i].Number) + "[.)]*[ \t\v\r\n\f]*[A-Za-z]*"
+		// find the user's answer to the corresponding question number
+		regexStr := "\n" + strconv.Itoa(answers[i].Number) + "[.)]*[ \t\v\r\n\f]*([A-Za-z]*)"
 		regex := regexp.MustCompile(regexStr)
-		userAnswer := regex.Find(bytes)
-
-		if userAnswer == nil {
+		ans := regex.FindStringSubmatch(string(bytes))
+		if len(ans) < 1 {
 			t.Errorf("%v %d: Answer not found.\n", sc.TestName, answers[i].Number)
 			sc.Dec()
-		} else {
-			r, _ := utf8.DecodeLastRune(userAnswer)
-			got, _ := utf8.DecodeLastRuneInString(strings.ToUpper(string(r)))
-			if got != answers[i].Want {
-				t.Errorf("%v %d: %q is incorrect.\n", sc.TestName, answers[i].Number, got)
-				sc.Dec()
-			}
+			continue
+		}
+		match := ans[1]
+		if len(match) == 0 {
+			t.Errorf("%v %d: Answer not found.\n", sc.TestName, answers[i].Number)
+			sc.Dec()
+			continue
+		}
+		if len(match) > 1 {
+			t.Errorf("%v %d: Multiple answers for question: %s\n", sc.TestName, answers[i].Number, match)
+			sc.Dec()
+			continue
+		}
+		got := strings.ToUpper(match)
+		if !strings.ContainsRune(got, answers[i].Want) {
+			t.Errorf("%v %d: %q is incorrect.\n", sc.TestName, answers[i].Number, got)
+			sc.Dec()
 		}
 	}
 }
